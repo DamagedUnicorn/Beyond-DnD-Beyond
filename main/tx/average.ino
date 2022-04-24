77ec122874aa0947ce5495819857969a019092ea
DamagedUnicorn <tpkzip@hotmail.com>
2022-04-24 13:39:32 +0200

    Delete average.ino

--- main/tx/average.ino
+++
@@ -1,7 +0,0 @@
-float average (float * array, int len)  // assuming array is int.
-{
-  long sum = 0L ;  // sum will be larger than an item, long for safety.
-  for (int i = 0 ; i < len ; i++)
-    sum += array [i] ;
-  return  ((float) sum) / len ;  // average will be fractional, so float may be appropriate.
-}
