377d684e7acc471214ddf8d8e96e6436fd2a52dd
DamagedUnicorn <tpkzip@hotmail.com>
2022-07-10 10:14:23 +0200

    Further translation

--- pythonImplementation/serialCommunication.py
+++ pythonImplementation/serialCommunication.py
@@ -7,13 +7,46 @@ Created on Thu Jul  7 17:39:20 2022
 """
 
 import serial
+import time
 
 class SerialCommunication:
     
     def __init__(self, port, baudRate):
         self.port = port
         self.baudRate = baudRate
-        self.ser = serial.Serial(self.port, self.baudRate)
+        self.isConnected = True
+        try:
+            self.ser = serial.Serial(self.port, self.baudRate)
+        except:
+            self.isConnected = False
         
-    def readData(self):
-        self.data = self.ser.readline()
+    #def readData(self):
+    #    return self.ser.readline()
+
+    def readData(self, gui):
+        # https://stackoverflow.com/questions/17553543/pyserial-non-blocking-read-loop
+        """
+        if (self.ser.inWaiting() > 0):
+            data = self.ser.read(self.ser.inWaiting()).decode('ascii')
+            try:
+                _ = int(data)
+                time.sleep(0.01)
+                #gui.setRoll(int(data))
+                #gui.window.update()
+                return data
+        """
+        isDataGood = False
+        if (self.ser.inWaiting() > 0):
+            data = self.ser.read(self.ser.inWaiting()).decode('ascii')
+            try:
+                _ = int(data)
+                isDataGood = True
+            except:
+                pass
+        time.sleep(0.01)
+        if isDataGood:
+            gui.setRoll(int(data))
+            gui.window.update()
+
+
+
